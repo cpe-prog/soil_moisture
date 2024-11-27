4c32e1cf4713130d3e1bc79548ffc95df6f536ab
Grian Gajila <griangajila2@gmail.com>
2024-11-27 17:29:37 +0800

    finalized

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,6 @@
 export default function NavBar() {
 	return (
-		<div className="flex w-full fixed h-20 items-center justify-start px-4 bg-blue-600">
+		<div className="flex w-full fixed h-20 items-center z-[1000] justify-start px-4 bg-blue-600">
 			<h1 className="font-bold text-xl customShadow">Smart Farming</h1>
 		</div>
 	);
